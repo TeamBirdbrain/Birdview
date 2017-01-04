2914076aa0e535c88f7a93ca4c06e88127c33f97
JamesWrigley <james@puiterwijk.org>
2017-01-04 14:35:21 +1300

    Assign copyright to Te Ropu Awhina, and update copyright date

--- src/ConnectDialog.hpp
+++ src/ConnectDialog.hpp
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2016 James Wrigley
+ * Copyright (C) 2017 Te Ropu Awhina (Victoria University of Wellington)
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
